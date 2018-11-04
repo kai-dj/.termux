07f2c3cff6d6f7acb909dbf8ba975fe9e3a6ca20
kai-dj <kai@kai.dj>
2018-11-04 19:18:13 +0100

    safe-volume-nag-remover root tool add

---
+++ root/tools/remove-safe-volume-nag.sh
@@ -0,0 +1,4 @@
+su -c 'mount -o rw,remount /product' &&
+su -c 'sed -i "s#ro.config.hw.security_volume=8#ro.config.hw.security_volume=0#g" /product/etc/prop/local.prop' &&
+su -c 'mount -o ro,remount /product' &&
+echo "nag should be removed"
