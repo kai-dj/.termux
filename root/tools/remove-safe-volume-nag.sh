92161e903e1a503667964a853faedb420c667047
kai-dj <kai@kai.dj>
2018-11-04 19:20:17 +0100

    add desc.

--- root/tools/remove-safe-volume-nag.sh
+++ root/tools/remove-safe-volume-nag.sh
@@ -1,3 +1,4 @@
+# root required! tsudo would only work, if /system/bin in $PATH - so "su -c" is used instead
 su -c 'mount -o rw,remount /product' &&
 su -c 'sed -i "s#ro.config.hw.security_volume=8#ro.config.hw.security_volume=0#g" /product/etc/prop/local.prop' &&
 su -c 'mount -o ro,remount /product' &&
